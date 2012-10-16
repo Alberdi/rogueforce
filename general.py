be79bf741dc626f16e82bd61afc6bf1f61830de7
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2012-10-16 23:59:44 +0200

    Added a bunch of new tactics.
    The most interesting is the disperse() function, because it's the
    only one that it's actually more than one line.

--- general.py
+++ general.py
@@ -15,8 +15,8 @@ class General(Minion):
     self.cd = []
     self.skills = [(skill.heal_target_minion, 100), (skill.heal_all_minions, 20), (skill.mine, 50), (skill.sonic_waves, 10, 3), (skill.water_pusher, 50)]
     self.skill_quotes =["Don't die!", "Heal you all men!", "Mine", "Sonic Waves", "Hidro Pump"]
-    self.tactics = [tactic.forward, tactic.stop, tactic.backward, tactic.go_sides, tactic.go_center]
-    self.tactic_quotes = ["Forward", "Fire", "Backward", "Go sides", "Go center"]
+    self.tactics = [tactic.forward, tactic.stop, tactic.backward, tactic.go_sides, tactic.go_center, tactic.attack_general, tactic.defend_general]
+    self.tactic_quotes = ["Forward", "Fire", "Backward", "Go sides", "Go center", "Attack", "Defend"]
     self.selected_tactic = self.tactics[0]
     self.command_tactic(0)
     self.strategies = []
--- tactic.py
+++ tactic.py
@@ -1,8 +1,33 @@
+from math import copysign
+
+def attack_general(minion):
+  minion.move(copysign(1, minion.bg.generals[(minion.side+1)%2].x - minion.x),
+              copysign(1, minion.bg.generals[(minion.side+1)%2].y - minion.y))
+
 def backward(minion):
   minion.move(-1 if minion.side == 0 else 1, 0)
 
+def defend_general(minion):
+  minion.move(copysign(1, minion.bg.generals[minion.side].x - minion.x),
+              copysign(1, minion.bg.generals[minion.side].y - minion.y))
+
+def disperse(minion):
+  r = 5
+  d = {(1,1):0, (-1,1):0, (-1,-1):0, (1,-1):0}
+  for i in range(-r, r+1):
+    for j in range(-r, r+1):
+      (x, y) = (minion.x+i, minion.y+j)
+      if not minion.bg.is_inside(x, y): continue
+      entity = minion.bg.tiles[(x, y)].entity
+      if entity is not None and entity != minion:
+        d[(copysign(1, i), copysign(1, j))] += 1
+  minion.move(*min(d, key=d.get))
+
 def forward(minion):
   minion.move(1 if minion.side == 0 else -1, 0)
+
+def go_bottom(minion):
+  minion.move(0, 1)
 
 def go_center(minion):
   minion.move(0, -1 if minion.y >= minion.bg.height/2 else 1)
@@ -10,5 +35,8 @@ def go_center(minion):
 def go_sides(minion):
   minion.move(0, 1 if minion.y >= minion.bg.height/2 else -1)
 
+def go_top(minion):
+  minion.move(0, -1)
+  
 def stop(minion):
   minion.next_action = 1
