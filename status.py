10892e7aab098eceaa8e1bc46494cebf30259479
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2012-10-18 23:45:16 +0200

    Statuses start on Entity, not on Minion.

--- status.py
+++ status.py
@@ -1,16 +1,16 @@
 class Status(object):
-  def __init__(self, minion, duration = 9999):
+  def __init__(self, entity, duration = 9999):
     self.duration = duration
-    self.minion = minion
-    if minion != None: # Not a prototype
-      self.minion.statuses.append(self)
+    self.entity = entity
+    if entity != None: # Not a prototype
+      self.entity.statuses.append(self)
   
-  def clone(self, minion):
-    return self.__class__(minion, self.duration)
+  def clone(self, entity):
+    return self.__class__(entity, self.duration)
 
   def end(self):
     self.duration = -1
-    self.minion.statuses.remove(self)
+    self.entity.statuses.remove(self)
 
   def tick(self): pass
 
@@ -22,21 +22,21 @@ class Status(object):
 
 class Poison(Status):
   # tbt = time between ticks
-  def __init__(self, minion, power, tbt = 0, ticks = 9999):
+  def __init__(self, entity, power, tbt = 0, ticks = 9999):
     #  Duration is not exact, it lasts a few more updates, but that shouldn't be a problem.
-    super(Poison, self).__init__(minion, ticks*(tbt+1))
+    super(Poison, self).__init__(entity, ticks*(tbt+1))
     self.tbt = tbt
     self.ticks = ticks
     self.power = power
     self.timer = 0
 
-  def clone(self, minion):
-    return self.__class__(minion, self.power, self.tbt, self.ticks)
+  def clone(self, entity):
+    return self.__class__(entity, self.power, self.tbt, self.ticks)
 
   def tick(self):
     self.timer -= 1
     if self.timer < 0:
-      self.minion.get_attacked(self)
+      self.entity.get_attacked(self)
       self.timer = self.tbt
       self.ticks -= 1
       if self.ticks == 0: self.duration = -1 # end
